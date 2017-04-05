74f93f587177a2f7e4e6237ac6663f266b6f684c
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-04-05 23:51:48 +0900

    no message

--- WEB/gemstone/displayer/admin.py
+++ WEB/gemstone/displayer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DailyRecord, Profile, SeasonRecord, TotalRecord
 
 # Register your models here.
+
+admin.site.register(DailyRecord)
+admin.site.register(Profile)
+admin.site.register(SeasonRecord)
+admin.site.register(TotalRecord)
--- WEB/gemstone/displayer/models.py
+++ WEB/gemstone/displayer/models.py
@@ -1,3 +1,96 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class DailyRecord(models.Model):
+    no = models.IntegerField(db_column='No', blank=True, null=True)  # Field name made lowercase.
+    date = models.CharField(db_column='Date', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    opponent = models.CharField(db_column='Opponent', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    pa = models.IntegerField(db_column='PA', blank=True, null=True)  # Field name made lowercase.
+    ab = models.IntegerField(db_column='AB', blank=True, null=True)  # Field name made lowercase.
+    h = models.IntegerField(db_column='H', blank=True, null=True)  # Field name made lowercase.
+    number_2b = models.IntegerField(db_column='2B', blank=True, null=True)  # Field name made lowercase. Field renamed because it wasn't a valid Python identifier.
+    number_3b = models.IntegerField(db_column='3B', blank=True, null=True)  # Field name made lowercase. Field renamed because it wasn't a valid Python identifier.
+    hr = models.IntegerField(db_column='HR', blank=True, null=True)  # Field name made lowercase.
+    rbi = models.IntegerField(db_column='RBI', blank=True, null=True)  # Field name made lowercase.
+    r = models.IntegerField(db_column='R', blank=True, null=True)  # Field name made lowercase.
+    sb = models.IntegerField(db_column='SB', blank=True, null=True)  # Field name made lowercase.
+    bb = models.IntegerField(db_column='BB', blank=True, null=True)  # Field name made lowercase.
+    so = models.IntegerField(db_column='SO', blank=True, null=True)  # Field name made lowercase.
+    avg = models.FloatField(db_column='AVG', blank=True, null=True)  # Field name made lowercase.
+    oba = models.FloatField(db_column='OBA', blank=True, null=True)  # Field name made lowercase.
+    sa = models.FloatField(db_column='SA', blank=True, null=True)  # Field name made lowercase.
+    ops = models.FloatField(db_column='OPS', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'daily_record'
+
+class Profile(models.Model):
+    no = models.AutoField(db_column='No', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(db_column='Name', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    number = models.CharField(db_column='Number', max_length=50)  # Field name made lowercase.
+    debut = models.CharField(db_column='Debut', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    born = models.CharField(db_column='Born', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    position = models.CharField(db_column='Position', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    body = models.CharField(db_column='Body', max_length=50, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'profile'
+
+
+class SeasonRecord(models.Model):
+    no = models.AutoField(db_column='No', primary_key=True)  # Field name made lowercase.
+    g = models.IntegerField(db_column='G', blank=True, null=True)  # Field name made lowercase.
+    pa = models.IntegerField(db_column='PA', blank=True, null=True)  # Field name made lowercase.
+    ab = models.IntegerField(db_column='AB', blank=True, null=True)  # Field name made lowercase.
+    h = models.IntegerField(db_column='H', blank=True, null=True)  # Field name made lowercase.
+    number_2b = models.IntegerField(db_column='2B', blank=True, null=True)  # Field name made lowercase. Field renamed because it wasn't a valid Python identifier.
+    number_3b = models.IntegerField(db_column='3B', blank=True, null=True)  # Field name made lowercase. Field renamed because it wasn't a valid Python identifier.
+    hr = models.IntegerField(db_column='HR', blank=True, null=True)  # Field name made lowercase.
+    rbi = models.IntegerField(db_column='RBI', blank=True, null=True)  # Field name made lowercase.
+    r = models.IntegerField(db_column='R', blank=True, null=True)  # Field name made lowercase.
+    sb = models.IntegerField(db_column='SB', blank=True, null=True)  # Field name made lowercase.
+    bb = models.IntegerField(db_column='BB', blank=True, null=True)  # Field name made lowercase.
+    so = models.IntegerField(db_column='SO', blank=True, null=True)  # Field name made lowercase.
+    avg = models.FloatField(db_column='AVG', blank=True, null=True)  # Field name made lowercase.
+    oba = models.FloatField(db_column='OBA', blank=True, null=True)  # Field name made lowercase.
+    sa = models.FloatField(db_column='SA', blank=True, null=True)  # Field name made lowercase.
+    ops = models.FloatField(db_column='OPS', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'season_record'
+
+
+class TotalRecord(models.Model):
+    no = models.AutoField(db_column='No', primary_key=True)  # Field name made lowercase.
+    g = models.IntegerField(db_column='G', blank=True, null=True)  # Field name made lowercase.
+    pa = models.CharField(db_column='PA', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    ab = models.IntegerField(db_column='AB', blank=True, null=True)  # Field name made lowercase.
+    h = models.IntegerField(db_column='H', blank=True, null=True)  # Field name made lowercase.
+    number_2b = models.IntegerField(db_column='2B', blank=True, null=True)  # Field name made lowercase. Field renamed because it wasn't a valid Python identifier.
+    number_3b = models.IntegerField(db_column='3B', blank=True, null=True)  # Field name made lowercase. Field renamed because it wasn't a valid Python identifier.
+    hr = models.IntegerField(db_column='HR', blank=True, null=True)  # Field name made lowercase.
+    rbi = models.IntegerField(db_column='RBI', blank=True, null=True)  # Field name made lowercase.
+    r = models.IntegerField(db_column='R', blank=True, null=True)  # Field name made lowercase.
+    sb = models.IntegerField(db_column='SB', blank=True, null=True)  # Field name made lowercase.
+    bb = models.IntegerField(db_column='BB', blank=True, null=True)  # Field name made lowercase.
+    so = models.IntegerField(db_column='SO', blank=True, null=True)  # Field name made lowercase.
+    avg = models.FloatField(db_column='AVG', blank=True, null=True)  # Field name made lowercase.
+    oba = models.FloatField(db_column='OBA', blank=True, null=True)  # Field name made lowercase.
+    sa = models.FloatField(db_column='SA', blank=True, null=True)  # Field name made lowercase.
+    ops = models.FloatField(db_column='OPS', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'total_record'

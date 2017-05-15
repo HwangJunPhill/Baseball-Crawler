d98a04e3d12a50e462898e8eab09431b40bc0a87
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-05-15 21:42:10 +0900

    no message

--- WEB/gemstone/displayer/views.py
+++ WEB/gemstone/displayer/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+from displayer.models import Profile, DailyRecord
+
+def index(request):
+    profile_list = Profile.objects.all().order_by('-no')[:5]
+    context = {'profile_list': profile_list}
+    return render(request, 'displayer/index.html', context)
+
+def data(request, profile_id):
+    profile = get_object_or_404(Profile, pk=profile_id)
+    daily = DailyRecord.objects.all().order_by('-no')[:5]
+    return render(request, 'displayer/data.html', {'profile': profile, 'daily':daily})

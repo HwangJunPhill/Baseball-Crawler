c348f7dec4eb4120e2527dc70fce33b4b5f92a8d
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-05-18 13:25:53 +0900

    Edit template, view

--- WEB/gemstone/displayer/views.py
+++ WEB/gemstone/displayer/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404, render
-from displayer.models import Profile, SeasonRecord, TotalRecord
+from displayer.models import Profile, SeasonRecord, TotalRecord, DailyRecord
 from django.http import JsonResponse
 
 def index(request):
@@ -8,26 +8,13 @@ def index(request):
     context = {'profile_list': profile_list}
     return render(request, 'displayer/index.html', context)
 
-# def data(request, profile_id):
-#     profile = get_object_or_404(Profile, pk=profile_id)
-#     season = SeasonRecord.objects.all().order_by('-no')[:5]
-#
-#     response = JsonResponse({'profile': profile, 'season': season})
-#     return render(request, 'displayer/data.html', response)
-
 
 def data(request, profile_id):
     profile = get_object_or_404(Profile, pk=profile_id)
     season = get_object_or_404(SeasonRecord, pk=profile_id)
     total = get_object_or_404(TotalRecord, pk=profile_id)
+    daily = get_object_or_404(DailyRecord, pk=profile_id)
 
-    context = {'profile': profile, 'season':season, 'total':total}
+    context = {'profile': profile, 'season':season, 'total':total, 'daily':daily}
 
     return render(request, 'displayer/data.html', context)
-
-# def data(request, profile_id):
-#     profile = get_object_or_404(Profile, pk=profile_id)
-#     seasons = SeasonRecord.objects.all().order_by('-no')[:5]
-#     context = {'profile': profile, 'season':seasons}
-#
-#     return render(request, 'displayer/data.html', context)
